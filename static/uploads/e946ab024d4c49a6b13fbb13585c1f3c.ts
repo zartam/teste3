85615bf204d38b8e7228871411b24df20f00f0d6
zartam <dcsrj@hotmail.com>
2025-02-08 19:29:20 -0300

    Add files via upload

---
+++ static/uploads/e946ab024d4c49a6b13fbb13585c1f3c.ts
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World</Text>
+      <Text>with Expo.io</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title:{
+    fontSize:28
+  }
+});
+
+npm create vite@latest hello-world -- --template react
+
+npm install
+
